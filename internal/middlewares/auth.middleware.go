2a5b5d75f612720042d27e498908bcdffb32dc49
hoangnh <hoangnh@hn.soc.one>
2025-09-17 15:51:00 +0700

    middleware + test

---
+++ internal/middlewares/auth.middleware.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ntva15z/go-ecommerce-backend-api/pkg/response"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		if token != "valid-token" {
+			response.ErrorResponse(c, response.ErrInvalidToken, "")
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
--- pkg/response/httpStatusCode.go
+++ pkg/response/httpStatusCode.go
@@ -3,9 +3,11 @@ package response
 const (
 	ErrCodeSuccess      = 20001 // success
 	ErrCodeParamInvalid = 20003 // email is invalid
+	ErrInvalidToken     = 30001 // token invalid
 )
 
 var msg = map[int]string{
 	ErrCodeSuccess:      "success",
 	ErrCodeParamInvalid: "email is invalid",
+	ErrInvalidToken:     "token is invalid",
 }
